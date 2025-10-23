5562868d22ebdc00772043d3580e626dae12e6eb
jkuwalek <jacob@shieldia.net>
2025-10-23 20:31:17 +0000

    Deploying to gh-pages from @ jkuwalek/jkuwalek.github.io@9b894c338a2cc9a1eb26918ee934fe41c57afcd1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jkuwalek/website",
+    "data-repo": "jkuwalek/jkuwalek.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
