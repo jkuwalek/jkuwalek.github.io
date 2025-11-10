5a8592f962507445ecb92f17234288d52ada31c4
jkuwalek <jacob@shieldia.net>
2025-11-10 08:57:46 +0000

    Deploying to gh-pages from @ jkuwalek/jkuwalek.github.io@0c1ba0091a25984a10a46182aecb16474dec72a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,6 +84,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/tabs/";
           
         },
+      },{id: "post-a-post-with-giscus-comments",
+        
+          title: "a post with giscus comments",
+        
+        description: "an example of a blog post with giscus comments",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2022/giscus-comments/";
+          
+        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
