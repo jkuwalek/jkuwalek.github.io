ffb50bbe4775c487b9c7b75bf437ca4897177697
jkuwalek <jacob@shieldia.net>
2025-11-10 08:29:47 +0000

    Deploying to gh-pages from @ jkuwalek/jkuwalek.github.io@6aadaf31375eefb6c72f5aa3188a66d42b860021 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,7 +96,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/template/announcement_2/";
+              window.location.href = "/news/mainpage/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
